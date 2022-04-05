ff6e3d5f5a7bc680b6ae7a265c852090989bb3b9
Nicholas Bollweg <nicholas.bollweg@gtri.gatech.edu>
2022-04-05 10:24:06 -0500

    clean up more paths

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from pathlib import Path
 HERE = Path(__file__).parent
 
 
-SRC = Path("jupyterlab_graph_lsp")
+SRC = Path("py_src/jupyterlab_graph_lsp")
 EXT_SRC = SRC / "labextension"
 PKG = EXT_SRC / "package.json"
 INSTALL = SRC / "install.json"
@@ -18,7 +18,9 @@ __install__ = json.loads(INSTALL.read_text(encoding="utf-8"))
 EXT_DEST = Path("share/jupyter/labextensions") / __js__["name"]
 
 
-DATA_FILES = [(str(EXT_DEST.as_posix()), ["jupyterlab_graph_lsp/install.json"])] + [
+DATA_FILES = [
+    (str(EXT_DEST.as_posix()), ["py_src/jupyterlab_graph_lsp/install.json"])
+] + [
     (
         str((EXT_DEST / path.relative_to(EXT_SRC).parent).as_posix()),
         [str(path.as_posix())],
