143feceaaa2c0a810370a6491f904acbe8aa3367
Nicholas Bollweg <nicholas.bollweg@gtri.gatech.edu>
2022-04-05 11:43:02 -0500

    overhaul plugins

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,87 +1,102 @@
 // Copyright (c) 2022 Dane Freeman.
 // Distributed under the terms of the Modified BSD License.
 
-import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
 import { ICodeMirror, Mode } from '@jupyterlab/codemirror';
 import {
   ILSPCodeExtractorsManager,
   ILSPFeatureManager,
 } from '@krassowski/jupyterlab-lsp';
 import { graphExtractors } from './extractors';
-import {
-  MODES_PLUGIN_ID,
-  EXTRACTOR_PLUGIN_ID,
-  graphqlIcon,
-  sparqlIcon,
-  sparulIcon,
-  turtleIcon,
-  DEBUG,
-} from './tokens';
+import { MODES_PLUGIN_ID, EXTRACTOR_PLUGIN_ID, DEBUG, IGraphModes } from './tokens';
+import { graphqlIcon, sparqlIcon, sparulIcon, turtleIcon } from './icons';
 
+/** Plugin metadata for @jupyrdf/jupyterlab-graph-lsp:ipython-graph */
 export const extractorPlugin: JupyterFrontEndPlugin<void> = {
   id: EXTRACTOR_PLUGIN_ID,
-  requires: [ILSPCodeExtractorsManager, ILSPFeatureManager],
-  activate: async (
-    app,
-    codeExtractors: ILSPCodeExtractorsManager,
-    lspf: ILSPFeatureManager
-  ) => {
-    // rely on LSPF to ensure plugin ordering
-    if (DEBUG) {
-      console.warn('loaded LSP features', lspf);
-    }
-
-    // install lsp extractors for magics
-    const promises = [];
-    for (const [language, extractors] of Object.entries(graphExtractors)) {
-      for (const extractor of extractors) {
-        codeExtractors.register(extractor, language);
-        promises.push(Mode.ensure(extractor.language));
-      }
-    }
-
-    Promise.all(promises)
-      .then((results) => console.table(results))
-      .catch(console.warn);
-  },
+  autoStart: true,
+  requires: [ILSPCodeExtractorsManager, ILSPFeatureManager, IGraphModes],
+  activate: activateExtractors,
 };
 
-export const modesPlugin: JupyterFrontEndPlugin<void> = {
-  id: MODES_PLUGIN_ID,
+/** Activation for @jupyrdf/jupyterlab-graph-lsp:ipython-graph */
+function activateExtractors(
+  app: JupyterFrontEnd,
+  codeExtractors: ILSPCodeExtractorsManager,
+  lspf: ILSPFeatureManager,
+  modes: IGraphModes
+) {
+  // rely on LSPF to ensure plugin ordering
+  DEBUG && console.warn('loaded LSP features', lspf);
+
+  modes.ready
+    .then(async () => {
+      DEBUG && console.warn('adding extractors');
+      // install lsp extractors for magics
+      const promises = [];
+      for (const [language, extractors] of Object.entries(graphExtractors)) {
+        for (const extractor of extractors) {
+          codeExtractors.register(extractor, language);
+          promises.push(Mode.ensure(extractor.language));
+        }
+      }
+      const results = await Promise.all(promises);
+      DEBUG && console.table(results);
+    })
+    .catch((err) => console.error('Failed to install extractors', err));
+}
+
+/** Plugin metadata for @jupyrdf/jupyterlab-graph-lsp:graph-modes */
+export const modesPlugin: JupyterFrontEndPlugin<IGraphModes> = {
   autoStart: true,
+  id: MODES_PLUGIN_ID,
+  provides: IGraphModes,
   requires: [ICodeMirror],
-  activate: async (app, cm: ICodeMirror) => {
+  activate: activateModes,
+};
+
+/** Activation for @jupyrdf/jupyterlab-graph-lsp:graph-modes */
+function activateModes(app: JupyterFrontEnd, cm: ICodeMirror): IGraphModes {
+  // add lab-specific files
+  app.docRegistry.addFileType({
+    name: 'GraphQL',
+    mimeTypes: ['application/graphql'],
+    extensions: ['.graphql'],
+    icon: graphqlIcon,
+  });
+  app.docRegistry.addFileType({
+    name: 'SPARQL',
+    mimeTypes: ['application/sparql-query'],
+    extensions: ['.sparql'],
+    icon: sparqlIcon,
+  });
+  app.docRegistry.addFileType({
+    name: 'sparul',
+    mimeTypes: ['application/sparql-update'],
+    extensions: ['.sparul'],
+    icon: sparulIcon,
+  });
+  app.docRegistry.addFileType({
+    name: 'Turtle',
+    mimeTypes: ['text/turtle'],
+    extensions: ['.ttl'],
+    icon: turtleIcon,
+  });
+
+  /** add the modes */
+  async function addModes() {
+    DEBUG && console.warn('adding modes');
     // ensures file type are available for documents
     const { installModes } = await import('./modes');
-
     await installModes(cm.CodeMirror);
+    DEBUG && console.warn('modes added');
+  }
 
-    // add lab-specific files
-    app.docRegistry.addFileType({
-      name: 'GraphQL',
-      mimeTypes: ['application/graphql'],
-      extensions: ['.graphql'],
-      icon: graphqlIcon,
-    });
-    app.docRegistry.addFileType({
-      name: 'SPARQL',
-      mimeTypes: ['application/sparql-query'],
-      extensions: ['.sparql'],
-      icon: sparqlIcon,
-    });
-    app.docRegistry.addFileType({
-      name: 'sparul',
-      mimeTypes: ['application/sparql-update'],
-      extensions: ['.sparul'],
-      icon: sparulIcon,
-    });
-    app.docRegistry.addFileType({
-      name: 'Turtle',
-      mimeTypes: ['text/turtle'],
-      extensions: ['.ttl'],
-      icon: turtleIcon,
-    });
-  },
-};
+  return { ready: addModes() };
+}
 
-export default [modesPlugin, extractorPlugin];
+const plugins: JupyterFrontEndPlugin<any>[] = [modesPlugin, extractorPlugin];
+
+DEBUG && console.warn('plugins', plugins);
+
+export default plugins;
--- src/tokens.ts
+++ src/tokens.ts
@@ -1,31 +1,26 @@
 // Copyright (c) 2022 Dane Freeman.
 // Distributed under the terms of the Modified BSD License.
+import { Token } from '@lumino/coreutils';
 
+/** The package namespace */
 export const NS = '@jupyrdf/jupyterlab-graph-lsp';
+
+/** The plugin ID for the language extractors (magic support) */
 export const EXTRACTOR_PLUGIN_ID = `${NS}:ipython-graph`;
-export const MODES_PLUGIN_ID = `${NS}:modes`;
 
-import { LabIcon } from '@jupyterlab/ui-components';
+/** The plugin ID for the CodeMirror modes */
+export const MODES_PLUGIN_ID = `${NS}:graph-modes`;
 
-import graphqlSvg from '../style/img/graphql.svg';
-import rdfSvg from '../style/img/rdf.svg';
+/** The plugin ID for the CodeMirror modes */
+export const IGraphModes = new Token<IGraphModes>(MODES_PLUGIN_ID);
 
-const BASE_COLOR_CLASS = 'jp-icon-contrast3';
+/** The public interface for the graph modes. */
+export interface IGraphModes {
+  /** A promise that resolves when the modes are installed. */
+  ready: Promise<void>;
+}
 
-export const graphqlIcon = new LabIcon({ svgstr: graphqlSvg, name: `${NS}:graphql` });
-export const turtleIcon = new LabIcon({
-  svgstr: rdfSvg.replace('RDF', 'TTL').replace(BASE_COLOR_CLASS, 'jp-icon-contrast1'),
-  name: `${NS}:turtle`,
-});
-export const sparqlIcon = new LabIcon({
-  svgstr: rdfSvg.replace('RDF', 'SPARQL'),
-  name: `${NS}:sparql`,
-});
-export const sparulIcon = new LabIcon({
-  svgstr: rdfSvg
-    .replace('RDF', 'SPARUL')
-    .replace(BASE_COLOR_CLASS, 'jp-icon-contrast2'),
-  name: `${NS}:sparul`,
-});
-
+/** A live debugging tool which increases verbosity. */
 export const DEBUG = window.location.href.indexOf('GRAPH_LSP_DEBUG') > -1;
+
+DEBUG && console.warn('GRAPH_LSP_DEBUG active');
